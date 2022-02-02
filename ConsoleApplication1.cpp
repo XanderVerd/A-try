4a627323ea3fb85c084f7a708acd0c49c1f04aac
Xander <verdonck.xander@gmail.com>
2022-02-02 15:41:14 +0100

    quadratic average, count, min, max added. Improved user interface.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,8 +5,9 @@ int main()
 {
 	//input numbers
 	const int Size = 20;
-	int numbers[Size];
+	float numbers[Size];
 	int count = 0;
+	std::cout << "Sumbit a value and press enter. When you are done submit anything else. A letter is enough." << "\n";
 	for (int i = 0; i < Size; i++) {
 		if (std::cin >> numbers[i]) {
 			count++;
@@ -15,6 +16,19 @@ int main()
 			break;
 		}
 	}
+
+	std::cout << "amount: " << count << "\n";
+
+	//min
+	float min = numbers[0];
+	// search num in inputArray from index 0 to elementCount-1 
+	for (int i = 1; i < count; i++) {
+		if (numbers[i] < min) {
+			min = numbers[i];
+		}
+	}
+	std::cout << "minimum: " << min << "\n";
+
 
 	//harmonic
 	float total = 0;
@@ -43,4 +57,26 @@ int main()
 	average = total / count;
 	//std::cout << average << "\n";
 	std::cout << "normal average: " << average << "\n";
+
+
+	//quadratic
+	total = 0;
+	for (int i = 0; i < count; i++) {
+		total += pow(numbers[i],2);
+		//std::cout << total << "\n";
+	}
+	average = sqrt(total / count);
+	//std::cout << average << "\n";
+	std::cout << "quadratic average: " << average << "\n";
+
+
+	//max
+	float max = numbers[0];
+	// search num in inputArray from index 0 to elementCount-1 
+	for (int i = 1; i < count; i++) {
+		if (numbers[i] > max) {
+			max = numbers[i];
+		}
+	}
+	std::cout << "maximum: " << max << "\n";
 }
